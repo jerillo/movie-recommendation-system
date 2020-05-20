3ef5ec8322785e71ae29b5854061e45a7ff5dd0f
jerillo <jesnineerillo@berkeley.edu>
2020-05-19 19:27:04 -0700

    updated show pages

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 3000;
 
 mongoose.connect('mongodb://localhost:27017/movies', {useNewUrlParser: true});
 app.use(bodyParser.urlencoded({extended: true}));
-app.use(express.static('public'));
+app.use(express.static(__dirname + '/public'));
 app.use(methodOverride('_method'))
 app.set('view engine', 'ejs');
 
