a39b1a8bacfde08a80d138e614768da8f770fc66
jerillo <jesnineerillo@berkeley.edu>
2020-05-23 22:58:25 -0700

    fixed bug to prevent double ratings

--- routes/ratings.js
+++ routes/ratings.js
@@ -35,10 +35,23 @@ router.get('/:id/new', middleware.isLoggedIn, (req, res) => {
 		if (!error && response.statusCode == 200) {
 			const data = JSON.parse(body);
 			if (data.Response === 'False') {
-				req.flash('error', 'Rating not found');
+				req.flash('error', 'Movie not found');
 				return res.redirect('/ratings');
 			}
-			res.render('ratings/new', { data: data });
+			Rating.findOne(
+				{
+					author: { id: req.user._id, username: req.user.username },
+					imdbID: req.params.id
+				},
+				(err, foundRating) => {
+					if (err || !foundRating) {
+						res.render('ratings/new', { data: data });
+					} else {
+						req.flash('error', 'Rating already exists');
+						res.redirect('/ratings');
+					}
+				}
+			);
 		}
 	});
 });
@@ -48,43 +61,56 @@ router.post('/:id', middleware.isLoggedIn, (req, res) => {
 	const imdbID = req.params.id;
 	const url =
 		`http://www.omdbapi.com/?apikey=${process.env.APIKEY}&i=` + imdbID;
-	request(url, (error, response, body) => {
-		if (!error && response.statusCode == 200) {
-			const data = JSON.parse(body);
-			if (data.Response === 'False') {
-				req.flash('error', 'Rating not found');
-				return res.redirect('/ratings');
+	Rating.findOne(
+		{
+			author: { id: req.user._id, username: req.user.username },
+			imdbID: req.params.id
+		},
+		(err, foundRating) => {
+			if (err || !foundRating) {
+				request(url, (error, response, body) => {
+					if (!error && response.statusCode == 200) {
+						const data = JSON.parse(body);
+						if (data.Response === 'False') {
+							req.flash('error', 'Rating not found');
+							return res.redirect('/ratings');
+						}
+						const rating = req.body.rating;
+						const comment = req.body.comment;
+						const newRating = {
+							title: data.Title,
+							year: data.Year,
+							imdbID: data.imdbID,
+							poster: data.Poster,
+							runtime: parseFloat(data.Runtime),
+							rating: rating,
+							comment: comment,
+							author: {
+								id: req.user._id,
+								username: req.user.username
+							}
+						};
+						if (isNaN(newRating.runtime)) {
+							newRating.runtime = 0;
+						}
+						// Create a new rating and save to ratings DB
+						Rating.create(newRating, (err, newlyCreated) => {
+							if (err) {
+								console.log(err);
+							} else {
+								req.user.ratings.push(newlyCreated);
+								req.user.save();
+								res.redirect('/ratings');
+							}
+						});
+					}
+				});
+			} else {
+				req.flash('error', 'Rating already exists');
+				res.redirect('/ratings');
 			}
-			const rating = req.body.rating;
-			const comment = req.body.comment;
-			const newRating = {
-				title: data.Title,
-				year: data.Year,
-				imdbID: data.imdbID,
-				poster: data.Poster,
-				runtime: parseFloat(data.Runtime),
-				rating: rating,
-				comment: comment,
-				author: {
-					id: req.user._id,
-					username: req.user.username
-				}
-			};
-			if (isNaN(newRating.runtime)) {
-				newRating.runtime = 0;
-			}
-			// Create a new rating and save to ratings DB
-			Rating.create(newRating, (err, newlyCreated) => {
-				if (err) {
-					console.log(err);
-				} else {
-					req.user.ratings.push(newlyCreated);
-					req.user.save();
-					res.redirect('/ratings');
-				}
-			});
 		}
-	});
+	);
 });
 
 // View rating
